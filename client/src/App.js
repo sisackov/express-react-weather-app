0cfb30d48bffbcd8b7dde919175d43184c7c1e44
sisackov <1123889+sisackov@users.noreply.github.com>
2022-01-16 12:40:33 +0200

    react done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState(null);
+    const [query, setQuery] = useState('Jerusalem'); //initially
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(
+                    'http://127.0.0.1:3001/weather?address=' + query
+                );
+                console.log('res', res);
+                setData(res.data);
+            } catch (err) {
+                console.log('err: ', err);
+            }
+        };
+
+        fetchData();
+    }, [query]);
+
+    const renderPage = () => {
+        if (!data) return <div>Loading...</div>;
+        return (
+            <div>
+                <div>Location: {data.location}</div>
+                <div>Forecast: {data.forecast}</div>
+            </div>
+        );
+    };
+
+    const handleInputKey = (e) => {
+        if (e.key === 'Enter') {
+            setQuery(e.target.value);
+        }
+    };
+
+    return (
+        <div className='App'>
+            <h1>Weather App</h1>
+            <input
+                type='text'
+                placeholder='Address'
+                onKeyPress={handleInputKey}
+            />
+            {renderPage()}
+        </div>
+    );
 }
 
 export default App;
--- src/app.js
+++ src/app.js
@@ -6,12 +6,14 @@ const forecast = require('./utils/forecast');
 const app = express();
 const port = process.env.PORT || 3001;
 
-// app.get('', (req, res) => {
-//     res.render('index', {
-//         title: 'Weather',
-//         name: 'Andrew Mead',
-//     });
-// });
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
 
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
